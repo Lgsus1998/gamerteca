c4120509c1693d0bf79a6778e83505bc76ddffe6
LGsus Parra <parravillegas01@gmail.com>
2024-12-01 19:34:04 -0600

    segundo commit

---
+++ src/pages/Header.jsx
@@ -0,0 +1,97 @@
+const Header = () => {
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="text-light bg-dark">
+                <h2 className="text-end">"El juego no tienen limites"</h2>
+                <div className="col" >
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsnPYEJFWNqUJMQVdXT1lLChBOhwbt9kBAoA&s" alt="mecanicorp"
+                    width={180}/>
+                </div>
+                <div>
+                    <h2 className="text-end">"La casa del gamer"</h2>
+                </div>
+
+             <div className="text-center">
+                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" to="/">ACERCA DE NOSOTROS</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" to="/biblioteca">GAMEPLAY</a>
+                            </li>
+                            <li className="nav-item">
+                            <a className="nav-link" to="/contactos">NUEVOS JUEGOS</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle"  id="agencias"  role="button" data-bs-toggle="dropdown" aria-expanded="false">Contactanos</a>
+                            <ul className="dropdown-menu" aria-labelledby="agencias" id="navbar">
+                            </ul>
+                            </li>
+                            <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" to="/conocemas">Conviertete en Streamer</a>
+                            </li>
+                        </ul>
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar"/>
+                            <button className="btn btn-outline-success" type="submit">Buscar</button>
+                        </form>
+    </div>
+  </div>
+
+</nav>
+                    
+                    </div>
+                    </div>
+                    <div>
+                        <h1>Noticias gamer de la actualidad</h1>
+                        <hr></hr>
+                        <h2>Rockstar habla de GTA 6 tras meses de silencio, y afirma que en el estudio "están preparando cosas que les van a volar la cabeza"</h2>
+                        <img src="https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2023/12/gta-6-3243960.jpg?tf=3840x" width={800}></img>
+                        <p className="text-black">"Muchas gracias por este increíble premio. Además, mandamos un enorme agradecimiento a todos los fans. Son lo más importante para nosotros. Significan mucho para Rockstar. Hay una cantidad increíble de personas haciendo cosas asombrosas con Grand Theft Auto 6 y están preparando cosas que os van a volar la cabeza. Así que es un verdadero honor poder estar aquí y aceptar este premio en nombre de todos. Ojalá más de nosotros pudiéramos estar aquí. Pero muchas gracias a todos, y sí, habrá más novedades próximamente", expresa y concluye el representante de Rockstar.
+
+Mientras un exdesarrollador de GTA 6 piensa que "Rockstar ha vuelto a subir el listón", la comunidad sigue esperando el tráiler 2  que vuelva a deleitarnos con más detalles de Vice City o de los personajes, Jason y Lucía. Quedan un par de semanas para que se cumpla un año desde que se publicó un primer tráiler de GTA 6 que ha roto varios récords Guiness en YouTube. La paciencia es la mejor arma de la comunidad, y al final la espera merecerá la pena.</p>
+
+<hr className="text-black"></hr>
+<h2 className="text-black">Black Myth Wukong lo está bordando. Es el título más vendido del año, y ahora se ha coronado como el mejor juego del año en los Golden Joystick Awards 2024</h2>
+<img src="https://i.blogs.es/04d3b5/qwewqe/1200_800.jpeg" width={800}></img>
+<p className="text-black">Los premios y galardones son importantes para las compañías y para la industria de los videojuegos ya que recompensan el esfuerzo de los desarrolladores al ofrecer un producto de calidad al mercado. Eventos como los Golden Joystick Awards no solo pueden impulsar las ventas de algunos títulos, sino que sirven como reconocimiento al trabajo realizado por su título durante varios años. En est2 2024, uno de los mejores sin duda ha sido Black Myth Wukong, y ya le podemos dar la enhorabuena a sus creadores porque ha obtenido el galardón más importante. 
+No ha sido como el 2023 con Baldur's Gate 3 arrasando en los Golden Joystick Awards, pero este año Black Myth Wukong ha obtenido el premio de 'Ultimate Game of the Year' en la edición de este año. El título de Game Science ha obtenido otro galardón aparte, aunque también debemos destacar a Helldivers 2 por recibir la distinción por ser el 'Mejor juego de consolas' a pesar de que su mayor éxito ha sido en PC vendiendo millones de unidades. 
+</p>
+
+<h2 className="text-black">Ganadores del Golden Joystick Awards 2024</h2>
+<p className="text-black">Mejor Diseño de Audio - Astro Bot<br></br>
+Mejor Banda Sonora - Final Fantasy 7: Rebirth<br></br>
+Mejor Narrativa - Final Fantasy 7: Rebirth<br></br>
+Mejor Juego Multijugador - Helldivers 2<br></br>
+Mejor Diseño Visual - Black Myth: Wukong<br></br>
+Mejor Juego Indie - Balatro<br></br>
+Mejor Juego Indie (Autoeditado) - Another Crab's Treasure<br></br>
+Estudio del Año - Team Asobi (Astro Bot)<br></br>
+Mejor interpretación principal - Cody Christian (Cloud, Final Fantasy 7: Rebirth)<br></br>
+Mejor interpretación secundaria - Briana White (Aerith, Final Fantasy 7: Rebirth)<br></br>
+Premio Juego Revelación (Elección de la Crítica) - Balatro<br></br>
+Premio al juego que "sigue jugándose" (Consolas y PC) - Minecraft<br></br>
+Premio al juego que "sigue jugándose" (Móviles) - Honkai: Star Rail<br></br>
+Juego de PC del Año - Satisfactory<br></br>
+Juego de Consolas del Año - Helldivers 2<br></br>
+Juego Más Esperado - GTA 6<br></br>
+Premio de la Crítica - Helldivers 2<br></br>
+Mejor Expansión de Juego - Elden Ring: Shadow of the Erdtree<br></br>
+Mejor Hardware de Videojuegos - Steam Deck OLED<br></br>
+Mejor Juego en Acceso Anticipado - Lethal Company<br></br>
+Mejor Tráiler de Juego - Helldivers 2<br></br>
+Premio de los Streamers - Chained Together<br></br>
+Mejor Adaptación de Videojuego - Fallout<br></br>
+Juego Definitivo del Año - Black Myth: Wukong</p><br></br>       
+                    </div>
+
+            </div>
+        </div>
+    )
+}
+export default Header;
